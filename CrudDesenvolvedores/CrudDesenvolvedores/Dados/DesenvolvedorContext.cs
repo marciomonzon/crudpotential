a2250682d4d37678088fc56545d86f460014e254
Marcio <marcio.pcmonzon@gmail.com>
2021-02-02 20:27:02 -0300

    primeiro commit

---
+++ CrudDesenvolvedores/CrudDesenvolvedores/Dados/DesenvolvedorContext.cs
@@ -0,0 +1,21 @@
+﻿using CrudDesenvolvedores.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudDesenvolvedores.Dados
+{
+    public class DesenvolvedorContext : DbContext
+    {
+        public DesenvolvedorContext(DbContextOptions<DesenvolvedorContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+           => optionsBuilder.UseNpgsql("Host=localhost;Database=postgres;Username=postgres;Password=123456");
+
+        public DbSet<Desenvolvedor> Desenvolvedor { get; set; }
+    }
+}
---
+++ CrudDesenvolvedores/CrudDesenvolvedores/Models/Desenvolvedor.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace CrudDesenvolvedores.Models
+{
+    public class Desenvolvedor
+    {
+        public int DesenvolvedorId { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório", AllowEmptyStrings = false)]
+        [StringLength(100, MinimumLength = 3, ErrorMessage = "O tamanho máximo permitido é 100 caracteres")]
+        public string Nome { get; set; }
+
+        [StringLength(1, MinimumLength = 1, ErrorMessage = "O tamanho máximo permitido é 1 caracter")]
+        [Required(ErrorMessage = "O campo Sexo é obrigatório", AllowEmptyStrings = false)]
+        public string Sexo { get; set; }
+
+        [Required(ErrorMessage = "O campo Idade é obrigatório", AllowEmptyStrings = false)]
+        public int Idade { get; set; }
+
+        [StringLength(100, ErrorMessage = "O tamanho máximo permitido é 100 caracteres")]
+        public string Hobby { get; set; }
+
+        [Required(ErrorMessage = "O campo Data de Nascimento é obrigatório", AllowEmptyStrings = false)]
+        public DateTime DataDeNascimento { get; set; }
+    }
+}
