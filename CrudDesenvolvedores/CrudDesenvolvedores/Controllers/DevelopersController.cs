fcbc91ccbcfd5b66e39804494c8b2e516f6b1799
Marcio <marcio.pcmonzon@gmail.com>
2021-02-03 20:38:07 -0300

    post e put

--- CrudDesenvolvedores/CrudDesenvolvedores/Controllers/DevelopersController.cs
+++ CrudDesenvolvedores/CrudDesenvolvedores/Controllers/DevelopersController.cs
@@ -1,13 +1,12 @@
-﻿using System;
+﻿using CrudDesenvolvedores.Dados;
+using CrudDesenvolvedores.Models;
+using CrudDesenvolvedores.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using CrudDesenvolvedores.Dados;
-using CrudDesenvolvedores.Enums;
-using CrudDesenvolvedores.Models;
-using CrudDesenvolvedores.Resolvedores;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+using System.Net;
 
 namespace CrudDesenvolvedores.Controllers
 {
@@ -17,25 +16,26 @@ namespace CrudDesenvolvedores.Controllers
     public class DevelopersController : ControllerBase
     {
         private readonly DesenvolvedorContext _context;
+        private readonly ServicoDesenvolvedor _servicoDesenvolvedor;
 
         public DevelopersController(DesenvolvedorContext context)
         {
             _context = context;
+            _servicoDesenvolvedor = new ServicoDesenvolvedor();
         }
 
         private IActionResult PesquisarPeloNome(string nome)
         {
-            var retorno = _context.Desenvolvedor?
-           .Where(x => x.Nome == nome)?.ToList();
+            var retorno = _servicoDesenvolvedor
+                .ObterDesenvolvedoresPeloNome(_context, nome);
 
-            if (retorno?.Count == 0)
-            {
-                return NotFound($"Nenhum desenvolvedor encontrado! Nome informado: {nome}");
-            }
-            return Ok(retorno);
+            if (retorno?.Count > 0)
+                return Ok(retorno);
+
+            return NotFound($"Nenhum desenvolvedor encontrado! Nome informado: {nome}");
         }
 
-        private List<Desenvolvedor> RetornarComPaginacao(int? page = null, int? pageSize = 5)
+        private List<Desenvolvedor> RetornarPesquisaComPaginacao(int? page = null, int? pageSize = 5)
         {
             var comPaginacao = _context.Desenvolvedor?
                   .Skip(((int)page - 1) * (int)pageSize)
@@ -45,6 +45,7 @@ namespace CrudDesenvolvedores.Controllers
             return comPaginacao;
         }
 
+        // /developers
         [HttpGet]
         public IActionResult ObterDesenvolvedores(string nome = "", int? page = null, int? pageSize = 5)
         {
@@ -56,7 +57,7 @@ namespace CrudDesenvolvedores.Controllers
                     return PesquisarPeloNome(nome);
 
                 if (page > 0)
-                    desenvolvedores = RetornarComPaginacao(page, pageSize);
+                    desenvolvedores = RetornarPesquisaComPaginacao(page, pageSize);
                 else
                     desenvolvedores = _context.Desenvolvedor?.ToList();
 
@@ -65,29 +66,116 @@ namespace CrudDesenvolvedores.Controllers
 
                 return NotFound("Nenhum desenvolvedor cadastrado!");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                return StatusCode(500);
+                return new ObjectResult(ex.Message) { StatusCode = (int)500 };
             }
         }
 
+        // /developers/id
         [HttpGet("{id}")]
         public IActionResult ObterDesenvolvedorPorId(int id)
         {
             try
             {
-                var desenvolvedores = _context.Desenvolvedor
-                    ?.Where(x => x.DesenvolvedorId == id)
-                    ?.ToList();
-                if (desenvolvedores?.Count > 0)
+                if (id == 0)
+                    return BadRequest("Código deve ser maior que 0");
+
+                var desenvolvedor = _servicoDesenvolvedor
+                    .ObterDesenvolvedor(_context, id);
+
+                if (desenvolvedor != null)
                 {
-                    return Ok(desenvolvedores);
+                    return Ok(desenvolvedor);
                 }
                 return NotFound($"Nenhum desenvolvedor cadastrado! Parametro informado: {id}");
             }
-            catch (Exception)
+            catch (Exception ex)
             {
-                return StatusCode(500);
+                return new ObjectResult(ex.Message) { StatusCode = (int)500 };
+            }
+        }
+
+        // /developers
+        [HttpPost]
+        public IActionResult InserirDesenvolvedor(Desenvolvedor desenvolvedor)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var existe = _servicoDesenvolvedor.
+                        VerificarSeDesenvolvedorExiste(_context, desenvolvedor);
+
+                    if (existe)
+                        return BadRequest("Este desenvolvedor já existe!");
+
+                    _context.Desenvolvedor.Add(desenvolvedor);
+                    _context.SaveChanges();
+                    return Ok($"O Desenvolvedor {desenvolvedor.Nome} foi cadastrado com sucesso!");
+                }
+
+                return BadRequest();
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult(ex.Message) { StatusCode = (int)500 };
+            }
+        }
+
+        // /developers/id
+        [HttpDelete("{id}")]
+        public IActionResult ExcluirDesenvolvedor(int id)
+        {
+            try
+            {
+                if (id == 0)
+                    return BadRequest("Código deve ser maior que 0");
+
+                var desenvolvedor = _servicoDesenvolvedor
+                    .ObterDesenvolvedor(_context, id);
+
+                if (desenvolvedor != null)
+                {
+                    _context.Desenvolvedor.Remove(desenvolvedor);
+                    _context.SaveChanges();
+                    return Ok($"Desenvolvedor excluído com sucesso. Nome do Desenvolvedor: {desenvolvedor.Nome}");
+                }
+                else
+                    return NotFound($"Desenvolvedor não encontrado! Código informado: {id}");
+
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult(ex.Message) { StatusCode = (int)500 };
+            }
+        }
+
+        // /developers/id
+        [HttpPut("{id}")]
+        public IActionResult AtualizarDesenvolvedor(int id, Desenvolvedor desenvolvedor)
+        {
+            try
+            {
+                if (id == 0)
+                    return BadRequest("Código deve ser maior que 0");
+
+                var desenvolvedorEncontrado = _servicoDesenvolvedor
+                     .ObterDesenvolvedor(_context, id);
+
+                if (desenvolvedorEncontrado != null)
+                {
+                    _context.Entry(desenvolvedor).State = EntityState.Modified;
+                    _context.SaveChanges();
+                    return Ok($"Desenvolvedor atualizado com sucesso. Nome do Desenvolvedor: {desenvolvedor.Nome}");
+                }
+                else
+                    return NotFound($"Desenvolvedor não encontrado! Código informado: {id}");
+
+            }
+            catch (Exception ex)
+            {
+                return new ObjectResult(ex.Message) { StatusCode = (int)500 };
             }
         }
     }
---
+++ CrudDesenvolvedores/CrudDesenvolvedores/Servicos/ServicoDesenvolvedor.cs
@@ -0,0 +1,45 @@
+﻿using CrudDesenvolvedores.Dados;
+using CrudDesenvolvedores.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CrudDesenvolvedores.Services
+{
+    public class ServicoDesenvolvedor
+    {
+        public Desenvolvedor ObterDesenvolvedor(DesenvolvedorContext contexto, int idDesenvolvedor)
+        {
+            var desenvolvedor = contexto.Desenvolvedor
+                    ?.Where(x => x.DesenvolvedorId == idDesenvolvedor)
+                    ? .AsNoTracking()
+                    ?.FirstOrDefault();
+
+            return desenvolvedor;
+        }
+
+        public List<Desenvolvedor> ObterDesenvolvedoresPeloNome(DesenvolvedorContext contexto, string nome)
+        {
+            var desenvolvedores = contexto.Desenvolvedor?
+          .Where(x => x.Nome == nome)?
+          .AsNoTracking()?
+          .ToList();
+
+            return desenvolvedores;
+        }
+
+        public bool VerificarSeDesenvolvedorExiste(DesenvolvedorContext contexto, Desenvolvedor desenvolvedor)
+        {
+            var existe = contexto.Desenvolvedor?
+                .Any(x => x.Nome == desenvolvedor.Nome
+                && x.Idade == desenvolvedor.Idade
+                && x.Sexo == desenvolvedor.Sexo);
+
+            if (existe != null)
+                return (bool)existe;
+
+            return false;
+        }
+    }
+}
